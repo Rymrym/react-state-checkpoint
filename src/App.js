d83a6db729a512b5ae7eeed04c64a533ba236ef9
RymHaouachi <rym.haouachi.ing@gmail.com>
2021-01-09 10:56:22 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from 'react'
+import yoda from './yoda.jpeg'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {person : {fullName : "Rym Haouachi", bio :"This is Rym Haouachi", imgSrc: yoda,  profession : "Developer" }, show: true, counter : 0};
+  }
+
+  
+
+  showPerson = () =>(
+    
+      this.setState({show : !this.state.show})
+      
+    
+   
+);
+
+
+componentDidMount(){
+  setInterval(() => { this.setState({counter : this.state.counter +1})
+    
+  }, 1000);
+}
+
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {this.state.show?  <>
+    <img src = {this.state.person.imgSrc} alt =""></img>
+    <h1 >{this.state.person.fullName}</h1>
+     <h1 >{this.state.person.bio}</h1>
+     <h1 >{this.state.person.profession}</h1>
+     <h1>{this.state.counter}</h1></>: <></>}
+     <button onClick = {this.showPerson}>Click me</button>
     </div>
   );
+}
 }
 
 export default App;
